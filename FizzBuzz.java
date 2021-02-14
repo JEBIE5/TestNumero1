b4ede1244eac4c773045fd46e5923c8e6e81a832
JEFF <jeffbienvenue@gmail.com>
2021-02-14 03:17:40 -0500

    Ajout du programme FizzBuzz en java

---
+++ FizzBuzz.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Entrer un chiffre entre 1 a 100");
+        try{
+        int chiffre = myObj.nextInt();
+
+            if (chiffre%3 == 0 && chiffre % 5== 0){
+                System.out.println("FizzBuzz"); }
+
+            else if (chiffre%3 == 0 ){ System.out.println("Fizz"); }
+
+            else if ( chiffre % 5 == 0){ System.out.println("Buzz"); }
+
+            else if (chiffre < 0 || chiffre > 100 ) { System.out.println("La " +
+                    "valeur entre est invalide Bonne journee"); }
+        }catch(Exception e) {
+            System.out.println("La valeur entre est invalide Bonne journee");
+        }
+    }
+}
